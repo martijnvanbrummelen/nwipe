d01b00b4f82ea1899eee1f0ffb47ebc6ac7e5dc2
martijn van brummelen <martijn@brumit.nl>
2016-07-27 09:33:17 +0200

    Update Release 0.18

--- src/version.c
+++ src/version.c
@@ -4,11 +4,11 @@
  * used by configure to dynamically assign those values 
  * to documentation files.
  */
-const char *version_string = "0.17";
+const char *version_string = "0.18";
 const char *program_name   = "nwipe";
-const char *author_name    = "Andy Beverley";
-const char *email_address  = "andy@andybev.com";
-const char *years          = "2012";
+const char *author_name    = "Martijn van Brummelen";
+const char *email_address  = "git@brumit.nl";
+const char *years          = "2016";
 const char *copyright      = "Copyright Darik Horn <dajhorn-dban@vanadac.com>\n\
 Modifications to original dwipe Copyright Andy Beverley <andy@andybev.com>\n\
 This is free software; see the source for copying conditions.\n\
