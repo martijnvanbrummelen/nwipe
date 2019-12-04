b102bee6fae27594ebe4a8a12eeaf21cb9ea5524
PartialVolume <nick.craig.law@gmail.com>
2019-12-04 22:09:12 +0000

    Update version.c
    Make the change in both version strings.

--- src/version.c
+++ src/version.c
@@ -4,7 +4,7 @@
  * used by configure to dynamically assign those values 
  * to documentation files.
  */
-const char *version_string = "0.26";
+const char *version_string = "0.27rc1";
 const char *program_name   = "nwipe";
 const char *author_name    = "Martijn van Brummelen";
 const char *email_address  = "git@brumit.nl";
