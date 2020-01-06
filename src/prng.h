7af9c90e3a89726d780325b2f66285ff61a2817e
louib <L0U13@protonmail.com>
2020-01-05 20:45:26 -0500

    Formatting prng module.

--- src/prng.h
+++ src/prng.h
@@ -14,7 +14,7 @@
  *
  *  You should have received a copy of the GNU General Public License along with
  *  this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. 
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  */
 
@@ -22,25 +22,25 @@
 #define PRNG_H_
 
 /* A chunk of random data. */
-typedef struct /* nwipe_entropy_t */
+typedef struct
 {
-	size_t  length;  /* Length of the entropy string in bytes.  */
-	u8*     s;       /* The actual bytes of the entropy string. */
+    size_t length;  // Length of the entropy string in bytes.
+    u8* s;  // The actual bytes of the entropy string.
 } nwipe_entropy_t;
 
-#define NWIPE_PRNG_INIT_SIGNATURE void** state, nwipe_entropy_t* seed
-#define NWIPE_PRNG_READ_SIGNATURE void** state, void* buffer, size_t count
+#define NWIPE_PRNG_INIT_SIGNATURE void **state, nwipe_entropy_t *seed
+#define NWIPE_PRNG_READ_SIGNATURE void **state, void *buffer, size_t count
 
 /* Function pointers for PRNG actions. */
-typedef int(*nwipe_prng_init_t)( NWIPE_PRNG_INIT_SIGNATURE );
-typedef int(*nwipe_prng_read_t)( NWIPE_PRNG_READ_SIGNATURE );
+typedef int ( *nwipe_prng_init_t )( NWIPE_PRNG_INIT_SIGNATURE );
+typedef int ( *nwipe_prng_read_t )( NWIPE_PRNG_READ_SIGNATURE );
 
 /* The generic PRNG definition. */
-typedef struct /* nwipe_prng_t */
+typedef struct
 {
-	const char*       label;  /* The name of the pseudo random number generator. */
-	nwipe_prng_init_t init;   /* Inialize the prng state with the seed.          */
-	nwipe_prng_read_t read;   /* Read data from the prng.                        */
+    const char* label;  // The name of the pseudo random number generator.
+    nwipe_prng_init_t init;  // Inialize the prng state with the seed.
+    nwipe_prng_read_t read;  // Read data from the prng.
 } nwipe_prng_t;
 
 /* Mersenne Twister prototypes. */
@@ -54,7 +54,4 @@ int nwipe_isaac_read( NWIPE_PRNG_READ_SIGNATURE );
 /* Size of the twister is not derived from the architecture, but it is strictly 4 bytes */
 #define SIZE_OF_TWISTER 4
 
-
 #endif /* PRNG_H_ */
-
-/* eof */
