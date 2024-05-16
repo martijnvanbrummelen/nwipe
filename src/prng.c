ad86d55bd1185a01000ccdab5e5f7cb9cc17692e
Michal Ambroz <723625+xambroz@users.noreply.github.com>
2024-05-16 03:55:15 +0200

    fix type error on i686 - uint64_t
    It is probably better to use the uint64_t type which is really used in the code for the respective generators

--- src/prng.c
+++ src/prng.c
@@ -269,7 +269,7 @@ int nwipe_add_lagg_fibonacci_prng_init( NWIPE_PRNG_INIT_SIGNATURE )
         *state = malloc( sizeof( add_lagg_fibonacci_state_t ) );
     }
     add_lagg_fibonacci_init(
-        (add_lagg_fibonacci_state_t*) *state, (long unsigned int*) ( seed->s ), seed->length / sizeof( long unsigned int ) );
+        (add_lagg_fibonacci_state_t*) *state, (uint64_t*) ( seed->s ), seed->length / sizeof( uint64_t ) );
 
     return 0;
 }
@@ -285,7 +285,7 @@ int nwipe_xoroshiro256_prng_init( NWIPE_PRNG_INIT_SIGNATURE )
         *state = malloc( sizeof( xoroshiro256_state_t ) );
     }
     xoroshiro256_init(
-        (xoroshiro256_state_t*) *state, (long unsigned int*) ( seed->s ), seed->length / sizeof( long unsigned int ) );
+        (xoroshiro256_state_t*) *state, (uint64_t*) ( seed->s ), seed->length / sizeof( uint64_t ) );
 
     return 0;
 }
