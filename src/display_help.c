4ed685abec3a3f87aa794e794489bce531549357
desertwitch <24509509+desertwitch@users.noreply.github.com>
2026-09-03 08:22:20 +0200

    fix: exclude display_help.c from clang-format

--- src/display_help.c
+++ src/display_help.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "ANSI-color-codes.h"
 
+/* clang-format off */
 void display_help()
 {
     /************************************************
@@ -210,3 +211,4 @@ void display_help()
     "        --exclude=/dev/disk/by-id/ata-XXXXXXXX\n" \
     "        --exclude=/dev/disk/by-path/pci-0000:00:17.0-ata-1\n\n");
 }
+/* clang-format on */
